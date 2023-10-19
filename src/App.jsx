1a12ba44081327ffb7b6bcb07954d029035f8855
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-10-19 16:01:19 +0530

    Added basic html to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@
 function App() {
   return (
     <>
-      sanity check?
+      <div className="landing">
+        <h1>Quizzical</h1>
+        <p>Some description if needed</p>
+        <button>Start game</button>
+      </div>
     </>
   )
 }
