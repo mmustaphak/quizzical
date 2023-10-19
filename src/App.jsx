231c66eb4d8ce0e2b486dd0916a772a27dac4aa6
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-10-19 19:27:55 +0530

    Moving landing code to seperate Landing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
+import Landing from "./components/Landing.jsx"
 
 function App() {
   return (
     <>
-      <div className="landing">
-        <h1>Quizzical</h1>
-        <p>Some description if needed</p>
-        <button>Start game</button>
-      </div>
+      <Landing/>
     </>
   )
 }
