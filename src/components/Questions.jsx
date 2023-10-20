4dcdfb668797732bd1c3a76b112fa9a75f7855e6
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-10-20 12:10:15 +0530

    Created handle click function

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -11,16 +11,27 @@ const Questions = ()=>{
             .catch(error => console.log(error))
     },[])
 
+    function handleClick(event){
+        console.log(event.target.id)
+    }
 
     const questionsArr = questions.map(item => {
         return (
             <div key={item.question} className="questions">
                 <h2>{item.question}</h2>
                 <div className="options">
-                <span>{item.correct_answer}</span>
-                <span>{item.incorrect_answers[0]}</span>
-                <span>{item.incorrect_answers[1]}</span>
-                <span>{item.incorrect_answers[2]}</span>
+                <span id="correct" onClick={(event) => handleClick(event)}>
+                    {item.correct_answer}
+                </span>
+                <span id="incorrect1" onClick={(event) => handleClick(event)}>
+                    {item.incorrect_answers[0]}
+                </span>
+                <span id="incorrect2" onClick={(event) => handleClick(event)}>
+                    {item.incorrect_answers[1]}
+                </span>
+                <span id="incorrect3" onClick={(event) => handleClick(event)}>
+                    {item.incorrect_answers[2]}
+                </span>
                 </div>
             <hr />
         </div>
