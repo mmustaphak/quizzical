06dc92eef127f9e7b7cf1dae7cf9c37eeef38d97
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-10-20 10:42:52 +0530

    Added basic html for next screen(questions)

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,25 @@ import Landing from "./components/Landing.jsx"
 function App() {
   return (
     <>
-      <Landing/>
+      {/* {<Landing/>} */}
+      <div className="questions">
+        <h2>How does one say goodbye in spanish ?</h2>
+        <div className="options">
+          <span>Adiós</span>
+          <span>Hola</span>
+          <span>Au Revoir</span>
+          <span>Salir</span>
+        </div>
+        <hr />
+      </div>
     </>
   )
 }
 
+/*
+  Next to do
+
+  make next screen
+
+initialise state to called isStarted. use state to conditionally render landing page and next screen */
 export default App
