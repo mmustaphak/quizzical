9ca8eb17db1f27a369e6ad47b77f067103faaef5
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-10-30 15:51:58 +0530

    Added dummy data for questions

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,51 @@ function App() {
   },[])
 
 
-  return (
+  return(
     <>
-      {isStarted ? "Test" : <Landing startGame={setIsStarted}/>}
+    <div className="question">
+      <div>
+        <h2>How would one say goodbye in Spanish?</h2>
+        <button>Adiós</button>
+          <button>Hola</button>
+        <button>Au Revoir</button>
+        <button>Salir</button>
+        <hr />
+      </div>
+        <div>
+          <h2>Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?</h2>
+          <button>Cabbage Patch Kids</button>
+          <button>Transformers</button>
+          <button>Care Bears</button>
+          <button>Rubik’s Cube</button>
+          <hr />
+        </div>
+        <div>
+          <h2>What is the hottest planet in our Solar System?</h2>
+          <button>Mercury</button>
+          <button>Venus</button
+          ><button>Mars</button>
+          <button>Saturn</button>
+          <hr />
+        </div>
+        <div>
+          <h2>In which country was the caesar salad invented?</h2>
+          <button>Italy</button>
+          <button>Portugal</button>
+          <button>Mexico</button>
+          <button>France</button>
+          <hr />
+        </div>
+        <div>
+          <h2>How Many Hearts Does An Octopus Have?</h2>
+          <button>One</button>
+          <button>Two</button>
+          <button>Three</button>
+          <button>Four</button>
+          <hr />
+        </div>
+    </div>
+      {/* {isStarted ? "Test" : <Landing startGame={setIsStarted}/>} */}
     </>
   )
 }
