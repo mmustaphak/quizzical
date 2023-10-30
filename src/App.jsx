38d19f502aa27a4c4e57094f2a67aec571d94541
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-10-30 15:16:44 +0530

    Moved Landing page to seperate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-
+import Landing from "./components/Landing.jsx"
 function App() {
   return (
     <>
-      <div className="landing">
-        <h1>Quizzical</h1>
-        <p>Some description if needed</p>
-        <button>Start game</button>
-      </div>
+      <Landing/>
     </>
   )
 }
---
+++ src/components/Landing.jsx
@@ -0,0 +1,10 @@
+const Landing = ()=>{
+    return(
+        <div className="landing">
+            <h1>Quizzical</h1>
+            <p>Some description if needed</p>
+            <button>Start game</button>
+        </div>
+    )
+}
+export default Landing
