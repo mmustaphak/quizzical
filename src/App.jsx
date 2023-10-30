61059b68ee9e378e0ab60c036829575be87ca175
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-10-30 15:31:16 +0530

    Made fetch reqeuest in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import Landing from "./components/Landing.jsx"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 function App() {
   
   const [isStarted,setIsStarted] = useState(false)
+  const [questions, setQuestions] = useState([])
+
+  useEffect(()=>{
+    fetch("https://opentdb.com/api.php?amount=5&category=9&type=multiple")
+      .then(data => data.json()).then(result => setQuestions(result.results))
+  },[])
+
 
   return (
     <>
