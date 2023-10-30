e5346cb0097bc69bc6f319f2b218b8fcd4f84cce
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-10-30 16:21:01 +0530

    Added basic styling for questions

--- src/App.jsx
+++ src/App.jsx
@@ -13,45 +13,55 @@ function App() {
 
   return(
     <>
-    <div className="question">
+    <div className="questions">
       <div>
         <h2>How would one say goodbye in Spanish?</h2>
-        <button>Adiós</button>
+        <div className="options">
+          <button>Adiós</button>
           <button>Hola</button>
-        <button>Au Revoir</button>
-        <button>Salir</button>
+          <button>Au Revoir</button>
+          <button>Salir</button>
+        </div>
         <hr />
       </div>
         <div>
           <h2>Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?</h2>
+        <div className="options">
           <button>Cabbage Patch Kids</button>
           <button>Transformers</button>
           <button>Care Bears</button>
           <button>Rubik’s Cube</button>
+          </div>
           <hr />
         </div>
         <div>
           <h2>What is the hottest planet in our Solar System?</h2>
-          <button>Mercury</button>
-          <button>Venus</button
-          ><button>Mars</button>
-          <button>Saturn</button>
+          <div className="options">
+            <button>Mercury</button>
+            <button>Venus</button>
+            <button>Mars</button>
+            <button>Saturn</button>
+          </div>
           <hr />
         </div>
         <div>
           <h2>In which country was the caesar salad invented?</h2>
-          <button>Italy</button>
-          <button>Portugal</button>
-          <button>Mexico</button>
-          <button>France</button>
+          <div className="options">
+            <button>Italy</button>
+            <button>Portugal</button>
+            <button>Mexico</button>
+            <button>France</button>
+          </div>
           <hr />
         </div>
         <div>
           <h2>How Many Hearts Does An Octopus Have?</h2>
-          <button>One</button>
-          <button>Two</button>
-          <button>Three</button>
-          <button>Four</button>
+          <div className="options">
+            <button>One</button>
+            <button>Two</button>
+            <button>Three</button>
+            <button>Four</button>
+          </div>
           <hr />
         </div>
     </div>
