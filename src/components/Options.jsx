7006f45a86ff33efd99a6ab440bbda84c81585ca
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-11-02 12:56:11 +0530

    Moved options to seperate component

---
+++ src/components/Options.jsx
@@ -0,0 +1,12 @@
+const Options = () => {
+    return (
+        <>
+            <button>Option1</button>
+            <button>Option2</button>
+            <button>Opitons3</button>
+            <button>Option4</button>
+        </>
+    )
+}
+
+export default Options
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,13 +1,12 @@
+import Options from "./Options";
+
 const Questions = ({questions})=>{
     const renderedQuestions = questions.map(question => {
         return (
             <div key={question.question}>
             <h2>{question.question}</h2>
             <div className="options">
-                <button>{question.correct_answer}</button>
-                <button>{question.incorrect_answers[0]}</button>
-                <button>{question.incorrect_answers[1]}</button>
-                <button>{question.incorrect_answers[2]}</button>
+                <Options/>
             </div>
             <hr />
         </div>
