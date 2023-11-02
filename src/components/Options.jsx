efd1533285a5e9dae237b625ee71e2a2445b81dd
Mohammed Mustapha Kura <75446049+mmustaphak@users.noreply.github.com>
2023-11-02 16:29:26 +0530

    Added styles for button when clicked

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react"
+
 const Options = ({correct_answer, incorrect_answers}) => {
-    console.log(correct_answer)
+
+    const [isClicked, seIsClicked] = useState({
+        option1: false,
+        options: false,
+        option3: false,
+        option4: false
+    })
+    
     return (
         <>
-            <button>{correct_answer}</button>
-            <button>{incorrect_answers[0]}</button>
-            <button>{incorrect_answers[1]}</button>
-            <button>{incorrect_answers[2]}</button>
+            <button
+                name="option1"
+                className={isClicked.option1 ? "clicked" : ""} 
+            >
+                {correct_answer}</button>
+            <button
+                name="option2"
+                className={isClicked.option2 ? "clicked" : ""}
+            >
+                {incorrect_answers[0]}</button>
+            <button
+                name="option3"
+                className={isClicked.option3 ? "clicked" : ""}
+            >
+                {incorrect_answers[1]}</button>
+            <button
+                name="option4"
+                className={isClicked.option4 ? "clicked" : ""}
+            >
+                {incorrect_answers[2]}</button>
         </>
     )
 }
